d580fa8ea80584148213ae2e2d69bfa48e6bc7fc
Scott Adams <scott.adams@eniro.com>
2019-11-15 22:51:50 +0100

    gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    pathPrefix: '/pubvendors-web',
     siteMetadata: {
         title: 'Pubvendors generator',
         description: 'Generate a pubvendors.json for IAB cookie consent solutions',
@@ -7,7 +8,7 @@ module.exports = {
         'gatsby-plugin-top-layout',
         'gatsby-plugin-react-helmet',
         'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp'
+        'gatsby-plugin-sharp',
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // 'gatsby-plugin-offline',
